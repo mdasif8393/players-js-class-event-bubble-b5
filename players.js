09b5282a288f99ca7835f38fe2d920a167ec39a5
mdasif8393 <md.asif.8393@gmail.com>
2022-08-17 11:43:47 +0600

    complete

---
+++ players.js
@@ -0,0 +1,11 @@
+const players = document.getElementsByClassName('player');
+for(const player of players) {
+    player.style.border = '1px solid red';
+    player.style.padding = '10px';
+}
+
+const addPlayer = () => {
+    const playersContainer = document.getElementById('players');
+    
+}
+document.querySelectorAll('#players .player:first-child').style.backgroundColor = red;
